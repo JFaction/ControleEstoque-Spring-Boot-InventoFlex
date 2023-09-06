4db4b28447b8b2abadc96ca044b83bd8457304e8
JFaction <juarezsouza.contato@hotmail.com>
2023-09-06 19:24:32 -0300

    atualizando projeto

--- src/main/java/com/InventoFlex/InventoFlexApplication.java
+++ src/main/java/com/InventoFlex/InventoFlexApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoFlexApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InventoFlexApplication.class, args);
 	}
